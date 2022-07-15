c73f667c4a8fe41fe830e78810ba737be26ded37
Patrizio Stavola <patrizio.stavola@gmail.com>
2022-07-15 19:17:07 +0200

    Ethers setup with RPC Provider

--- mint-and-burn/pages/_app.js
+++ mint-and-burn/pages/_app.js
@@ -1,7 +1,68 @@
+/* pages/__app.js */
 import '../styles/globals.css'
+import Link from 'next/link'
+import { css } from '@emotion/css'
+import 'easymde/dist/easymde.min.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <div>
+      <nav className={nav}>
+        <div className={header}>
+          <Link href="/">
+            <a>
+              <img
+                src='/logo.svg'
+                alt="React Logo"
+                style={{ width: '50px' }}
+              />
+            </a>
+          </Link>
+          <Link href="/">
+            <a>
+              <div className={titleContainer}>
+                <h2 className={title}>WBTC Mint and Burn</h2>
+                <p className={description}>The last 20 WBTC mint and burn requests</p>
+              </div>
+            </a>
+          </Link>
+        </div>
+      </nav>
+    </div>
+  )
 }
+
+const nav = css`
+  background-color: white;
+`
+
+const header = css`
+  display: flex;
+  border-bottom: 1px solid rgba(0, 0, 0, .075);
+  padding: 20px 30px;
+`
+
+const description = css`
+  margin: 0;
+  color: #999999;
+`
+
+const titleContainer = css`
+  display: flex;
+  flex-direction: column;
+  padding-left: 15px;
+`
+
+const title = css`
+  margin-left: 30px;
+  font-weight: 500;
+  margin: 0;
+`
+
+const link = css`
+  margin: 0px 40px 0px 0px;
+  font-size: 16px;
+  font-weight: 400;
+`
 
 export default MyApp
